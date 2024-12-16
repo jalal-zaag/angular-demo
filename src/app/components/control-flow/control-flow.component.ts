2e1bb3d8c96866908bb3faa26d0e1a6be61bac01
jalal-zaag <jalal@zaagsys.com>
2024-12-16 15:46:55 +0600

    control flow done

--- src/app/components/control-flow/control-flow.component.ts
+++ src/app/components/control-flow/control-flow.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './control-flow.component.css'
 })
 export class ControlFlowComponent {
+  isDivShow: boolean = false;
 
+  studentList: any[] = [
+    { name: 'aaa', city: "dhaka", isActive: true },
+    { name: 'bbb', city: "barishal", isActive: true },
+    { name: 'ccc', city: "Meghna", isActive: false },
+    { name: 'dd', city: "Sylhet", isActive: true },
+    { name: 'eee', city: "Raj", isActive: false }
+  ]
+
+  divShow(value: boolean) {
+    this.isDivShow = value;
+  }
 }
